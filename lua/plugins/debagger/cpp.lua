e056f39ec6d88b77ef6add1109e3cd11ff1117b4
lukavetskyi <lukavetskyihlib@gmail.com>
2024-06-13 21:48:52 +0200

    autodetect for debug config

--- lua/plugins/debagger/cpp.lua
+++ lua/plugins/debagger/cpp.lua
@@ -1,31 +1,51 @@
 -- yay -S codelldb-bin
 
+local function debug_config_program()
+  local f = io.open("debug.nvim.txt", 'r')
+  if f ~= nil then
+    local r = f:lines()()
+    f:close()
+    return r
+  else
+    return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+  end
+end
+
+local function debug_config_args()
+  local f = io.open("debug.nvim.txt", 'r')
+  if f ~= nil then
+    local i = f:lines()
+    i()
+    local r = i()
+    f:close()
+    return vim.split(r or "", " +")
+  else
+    local args_string = vim.fn.input('Arguments: ')
+    return vim.split(args_string, " +")
+  end
+end
+
 return function(dap)
-    dap.adapters.codelldb = {
-      type = 'server',
-      port = "${port}",
-      executable = {
-        command = '/usr/bin/codelldb',
-        args = { "--port", "${port}" },
+  dap.adapters.codelldb = {
+    type = 'server',
+    port = "${port}",
+    executable = {
+      command = '/usr/bin/codelldb',
+      args = { "--port", "${port}" },
 
-      }
     }
+  }
 
-    dap.configurations.cpp = {
-      {
-        name = "Launch file",
-        type = "codelldb",
-        request = "launch",
-        program = function()
-          return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-        end,
-        cwd = '${workspaceFolder}',
-        stopOnEntry = false,
-        args = function()
-          local args_string = vim.fn.input('Arguments: ')
-          return vim.split(args_string, " +")
-        end,
-      },
-    }
-    dap.configurations.c = dap.configurations.cpp
+  dap.configurations.cpp = {
+    {
+      name = "Launch file",
+      type = "codelldb",
+      request = "launch",
+      program = debug_config_program(),
+      cwd = '${workspaceFolder}',
+      stopOnEntry = false,
+      args = debug_config_args(),
+    },
+  }
+  dap.configurations.c = dap.configurations.cpp
 end
